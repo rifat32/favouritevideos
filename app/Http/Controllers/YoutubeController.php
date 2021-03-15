fa888d55b145ba4ead54f6fd1f21536f4c5f8142
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-03-15 10:10:39 +0600

    lenght fix

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -9,27 +9,34 @@ use Illuminate\Support\Str;
 
 class YoutubeController extends Controller
 {
-   public function index(){
-  $youtubes =  DB::table('videos')->where(['user_id' => Auth::user()->id,'website'=>'youtube'])->orderByDesc('id')->paginate(6);
-       return view('youtube',compact('youtubes'));
-   }
-   public function store(Request $request) {
-       $randomLink = rand(50,80);
-       $link = $request->link;
-       $link = substr($link,32);
-       $title = $request->title;
-       DB::table('videos')->insert([
-        'key' => Str::random($randomLink),
-        'link' => $link,
-        'title' => $title,
-        'user_id' => Auth::user()->id,
-        'website'=>'youtube'
-       ]
-       );
-       return back()->with('inserted','Youtube video has been inserted successfully.');
-   }
-   public function destroy($id){
-    DB::table('videos')->where(['id'=>$id])->delete();
-    return back()->with('deleted','Youtube video has been deleted successfully.');
-   }
+    public function index()
+    {
+        $youtubes =  DB::table('videos')->where(['user_id' => Auth::user()->id, 'website' => 'youtube'])->orderByDesc('id')->paginate(6);
+        return view('youtube', compact('youtubes'));
+    }
+    public function store(Request $request)
+    {
+        $randomLink = rand(50, 80);
+        $link = $request->link;
+
+        $link = substr($link, 32, 11);
+
+
+        $title = $request->title;
+        DB::table('videos')->insert(
+            [
+                'key' => Str::random($randomLink),
+                'link' => $link,
+                'title' => $title,
+                'user_id' => Auth::user()->id,
+                'website' => 'youtube'
+            ]
+        );
+        return back()->with('inserted', 'Youtube video has been inserted successfully.');
+    }
+    public function destroy($id)
+    {
+        DB::table('videos')->where(['id' => $id])->delete();
+        return back()->with('deleted', 'Youtube video has been deleted successfully.');
+    }
 }
